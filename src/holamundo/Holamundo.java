d97757ef12f4b8f5004272514a687114c7b012d3
user <user@com2pc12>
2018-05-29 04:50:00 -0600

    esdras 2

--- src/holamundo/Holamundo.java
+++ src/holamundo/Holamundo.java
@@ -8,7 +8,8 @@ public class Holamundo {
     public static void main(String[] args) {
 
     JOptionPane.showMessageDialog(null, "hello world"
-            + "esto no funcionna");
+            + "esto no funcionna esto "
+            + "asi funciona deverdad ");
     
     
     JOptionPane.showMessageDialog(null, "ya la cagamos en progra:v");
